ca1b7d13a3ba6b74728e10f1973fa796ad9b7180
Artaxerxes Antonio <61473701+xmelo@users.noreply.github.com>
2021-04-22 17:22:55 -0300

    Add files via upload

---
+++ Checkpoint Digital Business/funny_pets/src/main/java/org/funny_pets/model/Cadastro.java
@@ -0,0 +1,65 @@
+package org.funny_pets.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cadastro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String dataDeNascimento;
+	private String senha;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Cadastro [id=" + id + ", nome=" + nome + ", dataDeNascimento=" + dataDeNascimento + ", senha=" + senha
+				+ ", email=" + email + "]";
+	}
+
+}
---
+++ Checkpoint Digital Business/funny_pets/src/main/java/org/funny_pets/model/Contato.java
@@ -0,0 +1,55 @@
+package org.funny_pets.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contato {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String Sugestão;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSugestão() {
+		return Sugestão;
+	}
+
+	public void setSugestão(String sugestão) {
+		Sugestão = sugestão;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", nome=" + nome + ", Sugestão=" + Sugestão + ", email=" + email + "]";
+	}
+
+}
